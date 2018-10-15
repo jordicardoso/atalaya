eed55c555c925eb2b8a9c322f58b2db36995db6b
Jordi Cardoso <jordi.cardoso@bonpreu.cat>
2018-10-15 14:43:33 +0200

    bulk-insert

--- src/test/java/atalayaEIPTest.java
+++ src/test/java/atalayaEIPTest.java
@@ -28,7 +28,7 @@ public class atalayaEIPTest extends CamelBlueprintTestSupport {
         // Embed a JMS Broker for testing
         broker = new BrokerService();
         TransportConnector connector = new TransportConnector();
-        connector.setUri(new URI("tcp://localhost:61616"));
+        connector.setUri(new URI("tcp://localhost:61618"));
         broker.setBrokerName("activemq");
         broker.addConnector(connector);
         broker.setPersistent(false);
@@ -61,12 +61,13 @@ public class atalayaEIPTest extends CamelBlueprintTestSupport {
         InputStream inStream = getClass().getClassLoader().getResourceAsStream("t100.csv");
         String payload = context.getTypeConverter().convertTo(String.class, inStream);
 
-        Exchange exchange = template.request("netty4-http:http://localhost:8282/big/1.0/transfer/mysqlthedataDS",
+        Exchange exchange = template.request("netty4-http:http://localhost:8383/big/1.0/transfer/mysqlthedata",
                 new Processor() {
                     public void process(Exchange exchange) throws Exception {
                         exchange.getIn().setHeader(Exchange.HTTP_METHOD, "POST");
                         exchange.getIn().setHeader(Exchange.CONTENT_TYPE, "text/plain;charset=UTF-8");
-                        exchange.getIn().setHeader("insertSQL", "insert into T100 (SPRSL, MSG, MSGNR, TEXT, BIG_TRANSFERDATE, BIG_TRANSFERTIME, BIG_TRANSFERTSTAMP) values (':#${body[SPRSL]}', ':#${body[MSG]}', ':#${body[MSGNR]}', ':#${body[TEXT]}', ':#${body[BIG_TRANSFERDATE]}', ':#${body[BIG_TRANSFERTIME]}', ':#${body[BIG_TRANSFERTSTAMP]}')");
+                        //exchange.getIn().setHeader("insertSQL", "insert into T100 (SPRSL, MSG, MSGNR, TEXT, BIG_TRANSFERDATE, BIG_TRANSFERTIME, BIG_TRANSFERTSTAMP) values (1, :#${body[MSG]}, 11, 11111, 11111, 11111, 11111)");
+                        exchange.getIn().setHeader("insertSQL", "insert into T100 (SPRSL, MSG, MSGNR, TEXT, BIG_TRANSFERDATE, BIG_TRANSFERTIME, BIG_TRANSFERTSTAMP) values (:#SPRSL, :#$MSG, :#MSGNR, :#TEXT, :#BIG_TRANSFERDATE, :#BIG_TRANSFERTIME, :#BIG_TRANSFERTSTAMP)");
                         exchange.getIn().setBody(payload);
                     }
                 });
